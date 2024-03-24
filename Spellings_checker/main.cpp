4712bbd287f082926c0dbd6e3ecd483325a21af6
Dries Nuttin <dries.nuttin@hotmail.com>
2024-03-24 17:52:33 +0100

    code_base

--- Spellings_checker/main.cpp
+++ Spellings_checker/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string.h>
 
+/*
+* Calculating the Levenshtein distance between two strings
+* This is used to find possible substitutions and spelling mistakes
+* Author: Dries Nuttin
+*/
 using namespace std;
+
+int lev(char a, char b);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char a;
+    char b;
+    cout << "Please enter a string: ";
+    cin >> a;
+    cout << endl;
+
     return 0;
+}
+
+int lev(char a, char b)
+{
+    return a;
 }
